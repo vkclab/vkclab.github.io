f3e786516f48081cd269cce2e41b536e820199f4
Sunnia Ye <sye2@wellesley.edu>
2017-02-16 13:32:15 -0500

    fixed tweaks

--- js/people.js
+++ js/people.js
@@ -10,21 +10,18 @@ $.getJSON('https://vkclab.github.io/json/people.json', function(jsonData) {
 
         var person = "<div class='row'>";
 
-        person += "<div class='col-sm-4'>";
+        person += "<div class='col-sm-3'>";
         if ('photo' in jsonData[i]) {
-            person += "<img src='../img/people/" + jsonData[i]['photo'] + "'>";
+            person += "<img src='img/people/" + jsonData[i]['photo'] + "'>";
         }
         person += "</div>"; // close photo column
 
-        person += "<div class='col-sm-8'>";
-
-        person += "<p class='person_title'>" + jsonData[i]['nameyear'];
+        person += "<div class='col-sm-9'>";
+        person += "<p class='person_title'>" + jsonData[i]['nameyear'] + "</p>";
         if ('majors' in jsonData[i]) {
-            person += "<span class='person_major'>" + jsonData[i]['major'] + "</span>";
+            person += "<p class='person_major'>" + jsonData[i]['majors'] + "</p>";
         }
-        person += "</p>";
         person += "<p class='person_blurb'>" + jsonData[i]['blurb'] + "</p>";
-
         person += "</div>" // close blurb column
         person += "</div>" // close row div
 
